e245a92147db73e31a2e1811b8999830ec34e98d
rajesh bhartia <rbhartiamuk@gmmail.com>
2020-05-26 21:51:52 +0600

    Updates

---
+++ precache-manifest.ad53cac96c5ebc1e3bb588437c3fa356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab2bde2a40fd0f5ee1585cc41ae82c4",
+    "url": "/Rajeshbhartia/SimpleCrud/index.html"
+  },
+  {
+    "revision": "95c738fe4e6f188acaa3",
+    "url": "/Rajeshbhartia/SimpleCrud/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e853c27c2edfd0efb9d",
+    "url": "/Rajeshbhartia/SimpleCrud/static/js/2.b5568bdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Rajeshbhartia/SimpleCrud/static/js/2.b5568bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c738fe4e6f188acaa3",
+    "url": "/Rajeshbhartia/SimpleCrud/static/js/main.9de2c8d2.chunk.js"
+  },
+  {
+    "revision": "97113157b145aa18e58a",
+    "url": "/Rajeshbhartia/SimpleCrud/static/js/runtime-main.afe29a6a.js"
+  },
+  {
+    "revision": "8eab0a0ef6289338557f0213b384e17b",
+    "url": "/Rajeshbhartia/SimpleCrud/static/media/product.8eab0a0e.jpg"
+  }
+]);
