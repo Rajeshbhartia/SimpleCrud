79764b00516adc2c2b1b31e23a2f983e498735a2
rajesh bhartia <rbhartiamuk@gmmail.com>
2020-05-29 14:25:17 +0600

    connect with real server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import Products from "./Products"
 
+const domain = "https://limitless-falls-37987.herokuapp.com"
+
 class App extends Component {
   state = {
     proData: {},
     addToCartData: {}
   }
   async componentDidMount() {
-    const productUrl = "http://localhost:3010/product";
+    const productUrl = `${domain}/product`;
+    console.log(productUrl)
     const proResp = await fetch(productUrl)
     const proData = await proResp.json()
-
-    const addToCartUrl = "http://localhost:3010/addtocart";
+    const addToCartUrl = `${domain}/addtocart`;
     const addToCartResp = await fetch(addToCartUrl)
     let addToCartData = await addToCartResp.json()
-
     this.setState({ proData, addToCartData })
   }
 
   onQuery = async (key, value, action) => {
-    const url = "http://localhost:3010/addtocart";
+    const url =`${domain}/addtocart`;
     const resp = await fetch(url, {
       method: 'POST',
       headers: {
@@ -30,7 +31,6 @@ class App extends Component {
     let addToCartData = await resp.json()
     console.log(addToCartData)
     this.setState({ addToCartData })
-
   }
 
   render() {
