594f718ea5f7d5942829835e3c48d6228fe9970e
rajesh bhartia <rbhartiamuk@gmmail.com>
2020-05-29 14:35:15 +0600

    Updates

---
+++ precache-manifest.96b485577a7fca934e4b82da19b3cf75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c25667490e3a44ac5a2b3b86872fe8",
+    "url": "/SimpleCrud/index.html"
+  },
+  {
+    "revision": "ac454c0c4a5a567e0800",
+    "url": "/SimpleCrud/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1e853c27c2edfd0efb9d",
+    "url": "/SimpleCrud/static/js/2.b5568bdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SimpleCrud/static/js/2.b5568bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac454c0c4a5a567e0800",
+    "url": "/SimpleCrud/static/js/main.439ab3f9.chunk.js"
+  },
+  {
+    "revision": "72713d410398d8be2a59",
+    "url": "/SimpleCrud/static/js/runtime-main.36dffd9f.js"
+  },
+  {
+    "revision": "8eab0a0ef6289338557f0213b384e17b",
+    "url": "/SimpleCrud/static/media/product.8eab0a0e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rajeshbhartia/SimpleCrud/precache-manifest.80ef29f8f22e178746cf6874da0a094b.js"
+  "/SimpleCrud/precache-manifest.96b485577a7fca934e4b82da19b3cf75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rajeshbhartia/SimpleCrud/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SimpleCrud/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
